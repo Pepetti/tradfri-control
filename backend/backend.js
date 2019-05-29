c25ecf7f8b8302278b16bb8777af871c039ff16c
Oskari <oskari.kotajarvi@gmail.com>
2019-05-29 18:38:24 +0300

    Changed backend d variable to const from var

--- backend/backend.js
+++ backend/backend.js
@@ -1,35 +1,37 @@
-var io = require("socket.io")();
-var tradfri = require("node-tradfri-argon");
+var io = require('socket.io')();
+var tradfri = require('node-tradfri-argon');
 
-var d = new Date();
+const d = new Date();
 
 const port = 8000;
 
-io.on("connection", socket => {
-  console.log("User connected");
+io.on('connection', socket => {
+    console.log('User connected');
 
-  socket.on("action", function(action) {
-    console.log("Received request: " + action.type + "...");
+    socket.on('action', function(action) {
+        console.log('Received request: ' + action.type + '...');
 
-    switch (action.type) {
-      case "LOGIN_ATTEMPT":
-        console.log(action);
-        var obj = {
-            type: "LOGIN",
-            payload:{
-                loggedin: true,
-                user:{
-                    userName: action.payload,
-                    loggedinAt: d.getTime(),
-                    loggedoutAt: null
-                }
-            }
+        switch (action.type) {
+            case 'LOGIN_ATTEMPT':
+                console.log(action);
+                var obj = {
+                    type: 'LOGIN',
+                    payload: {
+                        loggedin: true,
+                        user: {
+                            userName: action.payload,
+                            loggedinAt: d.getTime(),
+                            loggedoutAt: null,
+                        },
+                    },
+                };
+                socket.emit('action', obj);
+                console.log(
+                    'Login success for user: ' + action.payload + '...',
+                );
         }
-        socket.emit("action", obj);
-        console.log("Login success for user: " + action.payload + "...");
-    }
-  });
+    });
 });
 
 io.listen(port);
-console.log("Listening on port: " + port);
+console.log('Listening on port: ' + port);
